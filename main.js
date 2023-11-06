4d6089d30fbca4ac17f9e7bde9a88a5b01155fdf
BrendanBouedec <bouedec97@gmail.com>
2023-11-06 14:01:41 +0100

    V3

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const helloWorld = <h1>Hello world!</h1>;
-
+const lastName = "bouedec";
+const firstName = "brendan";
+const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="name">{lastName.toUpperCase()}</span></h1>;
 ReactDOM.render(helloWorld, document.querySelector('#app'));
