49297ffdde2369ac03ecd5044d875ca6d4737a2f
BrendanBouedec <bouedec97@gmail.com>
2023-11-07 16:44:05 +0100

    V8

--- main.js
+++ main.js
@@ -1,39 +1,76 @@
-function UserGreeting(props) {
-    return <h1>Bienvenue !</h1>;
-  }
-  
-  function GuestGreeting(props) {
-    return <h1>Veuillez vous connecter</h1>;
-  }
-  function Greeting(props) {
-      const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+// function UserGreeting(props) {
+//     return <h1>Bienvenue !</h1>;
+//   }
 
-      const handleLogIn = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(true);
+//   function GuestGreeting(props) {
+//     return <h1>Veuillez vous connecter</h1>;
+//   }
+//   function Greeting(props) {
+//       const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+//       const handleLogIn = (e) => {
+//           e.preventDefault();
+//           setIsLoggedIn(true);
+//       }
+
+//       const handleLogOut = (e) => {
+//           e.preventDefault();
+//           setIsLoggedIn(false);
+//       }
+
+//     return(
+//         <React.Fragment>
+//             {isLoggedIn ? 
+//             <React.Fragment>
+//                 <UserGreeting />
+//                 <button onClick={handleLogOut}>Se déconnecter</button>
+//             </React.Fragment> : 
+//             <React.Fragment>
+//                 <GuestGreeting />
+//                 <button onClick={handleLogIn}>Se connecter</button>
+//             </React.Fragment> }
+//         </React.Fragment>
+//     )
+//   }
+
+//   ReactDOM.render(
+//     <Greeting />,
+//     document.querySelector('#app')
+//   );
+function App(props) {
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(usersObject => setUsers(usersObject))
+  }, [])
+
+  const [users, setUsers] = React.useState([]);
+
+
+
+  return (
+    <React.Fragment>
+      {
+        users.map((u) => (
+          <UserCard key={u.id} user={u} />
+        ))
       }
+    </React.Fragment>
+  )
+}
 
-      const handleLogOut = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(false);
-      }
+function UserCard({ user }) {
 
-    return(
-        <React.Fragment>
-            {isLoggedIn ? 
-            <React.Fragment>
-                <UserGreeting />
-                <button onClick={handleLogOut}>Se déconnecter</button>
-            </React.Fragment> : 
-            <React.Fragment>
-                <GuestGreeting />
-                <button onClick={handleLogIn}>Se connecter</button>
-            </React.Fragment> }
-        </React.Fragment>
-    )
-  }
-  
-  ReactDOM.render(
-    <Greeting />,
-    document.querySelector('#app')
-  );
+
+  return (
+    <ul className="user-card">
+      <li>{user.name}</li>
+      <li>{user.email}</li>
+      <li>{user.company.name}</li>
+      <li>{user.phone}</li>
+      <li>{user.website}</li>
+    </ul>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#
